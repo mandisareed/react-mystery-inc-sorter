cccbbb3f48bc42795ba91daa7b7d3c21635b1dcd
Mandisa Reed <mandisa.reed@gmail.com>
2020-08-06 17:42:56 -0700

    Updates

---
+++ precache-manifest.d9346ef5e3b62cb0edcd2fc3657107f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fae2c3e1ada3f9df5f1663c62ef8da",
+    "url": "/react-mystery-inc-sorter/index.html"
+  },
+  {
+    "revision": "f9bd1b083d19d44f7ad7",
+    "url": "/react-mystery-inc-sorter/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f9bd1b083d19d44f7ad7",
+    "url": "/react-mystery-inc-sorter/static/js/2.1448640d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mystery-inc-sorter/static/js/2.1448640d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f06b52b4c782c8f9df",
+    "url": "/react-mystery-inc-sorter/static/js/main.1edb8abd.chunk.js"
+  },
+  {
+    "revision": "1bf8c086288ca642389b",
+    "url": "/react-mystery-inc-sorter/static/js/runtime-main.255adb4c.js"
+  }
+]);
---
+++ static/js/main.1edb8abd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-mystery-inc-sorter"]=this["webpackJsonpreact-mystery-inc-sorter"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},9:function(e,t,n){"use strict";n.r(t);n(4);var r=n(0),a=n.n(r),c=n(2);var s=function(){return a.a.createElement("div",null,'"What\'s up!"')};n.n(c).a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(s,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.1edb8abd.chunk.js.map
